b45558eda465c4ca72b9f58c431cdc904686ce0a
IgorK97 <kt.wert.74@gmail.com>
2025-11-22 21:45:25 +0300

    PagedResult is modified

--- Chronolibris.Application/Models/PagedResult.cs
+++ Chronolibris.Application/Models/PagedResult.cs
@@ -20,40 +20,7 @@ namespace Chronolibris.Application.Models
         /// </summary>
         public IEnumerable<T> Items { get; init; } = [];
 
-        /// <summary>
-        /// Общее количество всех элементов, доступных в базе данных, вне зависимости от страницы.
-        /// Свойство доступно только для инициализации (<c>init</c>).
-        /// </summary>
-        //public int TotalCount { get; init; }
-
-        /// <summary>
-        /// Номер текущей страницы, которую возвращает данный результат (начиная с 1).
-        /// Свойство доступно только для инициализации (<c>init</c>).
-        /// </summary>
-        //public int Page { get; init; }
-
-        /// <summary>
-        /// Максимальное количество элементов, которое должно находиться на одной странице.
-        /// Свойство доступно только для инициализации (<c>init</c>).
-        /// </summary>
-        //public int PageSize { get; init; }
-
-        //public int TotalPages => (int)Math.Ceiling((double)TotalCount / PageSize);
-
-        //public bool HasNextPage => Page*PageSize < TotalCount;
-
-        //public bool HasPreviousPage => Page > 1;
-
-        //public PagedResult(IEnumerable<T> items, int totalCount, int page, int pageSize)
-        //{
-        //    Items = items;
-        //    TotalCount = totalCount;
-        //    Page = page;
-        //    PageSize = pageSize;
-        //}
-
-        //public long? Nextid { get; set; }
-        //public bool HasNextPage => Nextid is not null;
+        
         public int Limit { get; set; }
         public bool HasNext { get; set; }
 
