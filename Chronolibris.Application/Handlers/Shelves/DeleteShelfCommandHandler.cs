cf694df1baa054741af676f94fdba555d3c7597a
IgorK97 <kt.wert.74@gmail.com>
2026-04-11 15:31:44 +0300

    Fixed: delete shelf command handler

--- Chronolibris.Application/Handlers/Shelves/DeleteShelfCommandHandler.cs
+++ Chronolibris.Application/Handlers/Shelves/DeleteShelfCommandHandler.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Chronolibris.Application.Requests.Shelves;
+using Chronolibris.Domain.Exceptions;
 using Chronolibris.Domain.Interfaces;
 using MediatR;
 
@@ -19,9 +20,12 @@ namespace Chronolibris.Application.Handlers.Shelves
         public async Task<Unit> Handle(DeleteShelfCommand request, CancellationToken ct)
         {
             var shelf = await _uow.Shelves.GetByIdAsync(request.ShelfId, ct);
-            if (shelf == null || shelf.UserId != request.UserId)
-                return Unit.Value;
-            
+            if (shelf == null)
+                throw new ChronolibrisException("Книжная полка не найдена", ErrorType.NotFound);
+
+            if (shelf.UserId != request.UserId)
+                throw new ChronolibrisException("Нет прав на совершение данной операции", ErrorType.Forbidden);
+
             _uow.Shelves.Delete(shelf);
             await _uow.SaveChangesAsync(ct);
             return Unit.Value;
